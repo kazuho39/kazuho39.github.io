7425c62f6e601f16b185fcdc72330cfe5050fa83
kazuho <kazuho9039@gmail.com>
2025-04-10 21:25:58 +0900

    add: null object

---
+++ app/best-practice/null-object/page.tsx
@@ -0,0 +1,23 @@
+import fs from "fs";
+import path from "path";
+import { extractHeadings, Heading } from "@/lib/extractHeadings";
+import { markdownToHtml } from "@/lib/markdownToHtml";
+import LayoutMarkdownWithSidebar from "@/components/LayoutMarkdownWithSidebar";
+
+export default async function LaravelDirectoryStructurePage() {
+  const markdownFilePath = path.join(process.cwd(), "content", "best-practice", "null-object.md");
+  const markdownContent = fs.readFileSync(markdownFilePath, "utf8");
+
+  // Extract headings
+  const headings: Heading[] = extractHeadings(markdownContent);
+
+  // Convert Markdown to HTML
+  const contentHtml = await markdownToHtml(markdownContent);
+
+  return (
+    <>
+      <title>Null Object</title>
+      <LayoutMarkdownWithSidebar headings={headings} contentHtml={contentHtml} />
+    </>
+  );
+}
